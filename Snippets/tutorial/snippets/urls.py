16796ee2c46b34c1f73cc1a2d46a3067db936168
sergei-p <sergeidev2494@gmail.com>
2022-07-04 20:25:58 -0700

    Fixed 500 error by removing the basenames. Another way to solve the issue would be to change the the basename in the following way. "Snippets" to "Snippet" and "Users" to "User."

--- Snippets/tutorial/snippets/urls.py
+++ Snippets/tutorial/snippets/urls.py
@@ -9,8 +9,8 @@ from snippets import views
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
-router.register(r'snippets', views.SnippetViewSet,basename="snippets")
-router.register(r'users', views.UserViewSet,basename="users")
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
